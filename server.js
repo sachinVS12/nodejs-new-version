505d9526cea93afc924f2ecee9911a02498dcaef
Sachin <vs1sachi12@gmail.com>
2025-08-23 15:21:18 +0530

    first commit3

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
+
 const PORT = process.env.PORT;
+
+// Get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const server = http.createServer((req, res) =>{
-    //res.setHeader('Content-Type', 'text/html');
-    //res.statusCode = 404;
-    console.log(req.url);
-    console.log(req.method);
-    
-   res.writeHead(200, {'Content-Type': 'tex/html'});
-    res.end('<h1>Hello world!</h1>');
-//res.end(JSON.stringify({ message: 'Server Error'}));
+   try{
+    //check if GET request
+    if (req.method === 'GET') {
+      if(req.url === '/') {
+       let filepath = path.join(__dirname, 'public', 'index.html');  
+    } else if (req.url === '/about'){
+      filepath = path.join(__dirname, 'public', 'about.html');
+    } else {
+        throw new Error('Not Found');
+    }
+    //const data = await fs.readFile(filepath);
+    res.setHeader('Content-Type', 'text/html');
+    res.write(data);
+    res.end();
+    } else{
+        throw new Error('Method not allowed');
+    }
+   } catch (error) {
+     res.writeHead(500, {'Content-Type': 'tex/plain'});
+    res.end('<h1>server Error</h1>');
+   }
 });
 
 
