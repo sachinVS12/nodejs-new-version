a2172455334b3b139b7040d195c76ae6c9d35412
Sachin <vs1sachi12@gmail.com>
2025-08-23 11:14:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const { generateRandomNumber, celciusToFahrenheit} = require('./utils');
+
+console.log(`Random Number: ${generateRandomNumber()}`);
+
+console.log(`Clecius to fahrenhiet: ${celciusToFahrenheit(0)}`);
