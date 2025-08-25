a740bb171e8d6744918ccb5db02e1008dd1a7029
Sachin <vs1sachi12@gmail.com>
2025-08-25 18:12:16 +0530

    first commit8

--- fsDemo.js
+++ fsDemo.js
@@ -9,3 +9,16 @@ console.log(getPosts());
 
 console.log(`Posts Length: ${getPostsLength()}`);
 
+//signup and login
+
+// const { generateRandomNumber, celciusToFahrenheit} = require('./utils');
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+
+// console.log(`Clecius to fahrenhiet: ${celciusToFahrenheit(0)}`);
+import getPosts, { getPostsLength } from './postController.js';
+
+console.log(getPosts());
+
+console.log(`Posts Length: ${getPostsLength()}`);
+
